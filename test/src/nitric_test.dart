7f97d92a296ef37f3e92287693789aa9a6ae3990
Tim Holm <tim.holm@nitric.io>
2024-02-26 15:38:21 +1100

    add test stubs.

---
+++ test/src/nitric_test.dart
@@ -0,0 +1,31 @@
+import 'package:test/test.dart';
+
+void main() {
+  test('TODO: api test', () {
+
+  });
+
+  test('TODO: bucket test', () {
+
+  });
+
+  test('TODO: store test', () {
+
+  });
+
+  test('TODO: schedule test', () {
+
+  });
+
+  test('TODO: secret test', () {
+
+  });
+
+  test('TODO: topic test', () {
+
+  });
+
+  test('TODO: websocket test', () {
+
+  });
+}
