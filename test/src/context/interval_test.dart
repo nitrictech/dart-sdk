052f9d8e8bff2a973b81bac84c95dc45c777e9c9
Tim Holm <tim.holm@nitric.io>
2024-02-27 15:15:54 +1100

    format.

--- test/src/context/interval_test.dart
+++ test/src/context/interval_test.dart
@@ -1,10 +1,10 @@
 import 'package:nitric_sdk/src/context/common.dart';
-import 'package:nitric_sdk/src/nitric/proto/schedules/v1/schedules.pbgrpc.dart' as $pb;
+import 'package:nitric_sdk/src/nitric/proto/schedules/v1/schedules.pbgrpc.dart'
+    as $pb;
 import 'package:test/test.dart';
 
 void main() {
   test('Interval Request from ServerMessage', () {
-
     var msg = $pb.ServerMessage(
       id: 'id',
       intervalRequest: $pb.IntervalRequest(scheduleName: 'scheduleName'),
@@ -14,6 +14,5 @@ void main() {
 
     expect(req.id, msg.id);
     expect(req.req.scheduleName, msg.intervalRequest.scheduleName);
-
   });
 }
