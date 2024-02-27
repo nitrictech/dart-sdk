18e42589ddccfbbc5110afaf1fd37db99e9caff9
Tim Holm <tim.holm@nitric.io>
2024-02-27 15:14:42 +1100

    add test to populate coverage reports.

---
+++ test/src/context/interval_test.dart
@@ -0,0 +1,19 @@
+import 'package:nitric_sdk/src/context/common.dart';
+import 'package:nitric_sdk/src/nitric/proto/schedules/v1/schedules.pbgrpc.dart' as $pb;
+import 'package:test/test.dart';
+
+void main() {
+  test('Interval Request from ServerMessage', () {
+
+    var msg = $pb.ServerMessage(
+      id: 'id',
+      intervalRequest: $pb.IntervalRequest(scheduleName: 'scheduleName'),
+    );
+
+    IntervalContext req = IntervalContext.fromRequest(msg);
+
+    expect(req.id, msg.id);
+    expect(req.req.scheduleName, msg.intervalRequest.scheduleName);
+
+  });
+}
